7a2876e15df4db8aefbec0b0270d7ff11b4d7ba3
mfournial <mayeul.fournial@outlook.com>
2017-04-23 20:18:48 +0100

    minor changes to metamask dir

--- metamask-logo/example.js
+++ metamask-logo/example.js
@@ -1,4 +1,5 @@
-
+// Don't use this method as it gives no control over the fox
+// Leave it here in case I change my mind
 
 var createViewer = require('../index');
 
@@ -6,6 +7,6 @@ var viewer = createViewer({
   width: 0.4,
   height: 0.4,
   followMouse: true
-})
+});
 
 document.getElementById('fox').appendChild(viewer.container);
